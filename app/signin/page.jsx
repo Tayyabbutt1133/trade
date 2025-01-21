8736ea3b2552d269b29b3184dc1bd03431730535
Uzain Bin Mubarak <uzainmubarak@gmail.com>
2025-01-21 21:56:15 +0500

    remove SignInForm component from SignInPage

--- app/signin/page.jsx
+++ app/signin/page.jsx
@@ -9,7 +9,7 @@ export default function SignInPage() {
         <div className="text-center">
           <h2 className={`mt-6 text-3xl font-bold text-gray-900 ${fonts.montserrat}`}>Sign in to your account</h2>
         </div>
-        <SignInForm />
+        {/* <SignInForm /> */}
         <p className={`mt-2 text-center text-sm text-gray-600 ${fonts.montserrat}`}>
           Don't have an account?{" "}
           <Link href="/signup" className="font-medium text-[#37bfb1] hover:text-[#2ea89b]">
