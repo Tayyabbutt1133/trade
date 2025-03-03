72fcf7b96d051e1626f71c4a3f31231470dd8bce
Tayyeb Shahzad <tayyebbutt966@gmail.com>
2025-03-04 00:38:36 +0500

    no cache added

--- app/(dashboard)/dashboard/page.jsx
+++ app/(dashboard)/dashboard/page.jsx
@@ -1,5 +1,3 @@
-export const dynamic = 'force-dynamic';
-
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { fonts } from "@/components/ui/font";
 import { Users, Package, MessageSquare } from "lucide-react";
