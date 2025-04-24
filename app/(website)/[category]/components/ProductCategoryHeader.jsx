407d3d89f933116b7c37b1aed1663c440bfca7bf
Tayyeb Shahzad <tayyebbutt966@gmail.com>
2025-04-24 12:32:48 +0500

    count will not get showed from top business line headings as there is no actual data in that , doing this short term conditioning for now , it is not scalable

--- app/(website)/[category]/components/ProductCategoryHeader.jsx
+++ app/(website)/[category]/components/ProductCategoryHeader.jsx
@@ -2,6 +2,17 @@ import { fonts } from "@/components/ui/font";
 import { Button } from "@/components/ui/button";
 
 export default function ProductCategoryHeader({ category, totalProducts }) {
+  const categoriesToHideCount = [
+    "Paints and Coatings",
+    "Inks",
+    "Adhesives and Sealants",
+    "Construction Chemicals",
+    "Paper Industry",
+    "Lab. Equipment",
+  ];
+
+  const shouldShowCount = !categoriesToHideCount.includes(category);
+
   return (
     <div
       className={`flex w-full flex-col md:flex-row md:items-start md:justify-between space-y-6 md:space-y-0 ${fonts.montserrat} mb-6`}
@@ -9,9 +20,8 @@ export default function ProductCategoryHeader({ category, totalProducts }) {
       <section className="space-y-4">
         <div className="space-y-2">
           <h1 className="text-3xl md:text-5xl font-semibold text-gray-900 drop-shadow-[4px_4px_2px_rgba(0,0,0,0.25)]">
-            {category} ({totalProducts})
+            {category} {shouldShowCount && `(${totalProducts})`}
           </h1>
-          {/* <p className="text-xs text-gray-600">{`${totalProducts} Products`}</p> */}
         </div>
 
         <p className="text-gray-800 text-sm font-medium max-w-3xl">
